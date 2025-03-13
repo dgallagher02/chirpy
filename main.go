ca0fdd46f19af44e89495c30ee514941defe59c3
dgallagher02 <dgallagher02@gmail.com>
2025-03-13 08:48:00 -0400

    some metric hendling

--- main.go
+++ main.go
@@ -3,15 +3,55 @@ package main
 import (
 	"net/http"
 	"log"
+	"sync/atomic"
+	"fmt"
 )
 
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	apiCfg := apiConfig{}
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
-	srv := http.Server{Addr: ":8080", Handler: mux}
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
 
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
-	
+}
 
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hits: %v", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        // This is where we should increment the counter
+        cfg.fileserverHits.Add(1)
+        // Then call the next handler
+        next.ServeHTTP(w, r)
+    })
 }
